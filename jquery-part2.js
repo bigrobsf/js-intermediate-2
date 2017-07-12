50b80b3cd414132ef5edb99689f0359bd9588f99
Rob Conner <rjconner@mac.com>
2017-07-11 21:56:38 -0700

    Intermediate jQuery Exercise Part 2

--- jquery-part2.js
+++ jquery-part2.js
@@ -8,22 +8,21 @@
 $(function() {
   $('#add-row').on('click', addRow);
 
-
   function addRow() {
     let artist = $('#artist').val();
     let rating = $('#rating').val();
 
     let $newArtistTd = $('<td>');
+    $newArtistTd.addClass('text-center artist');
     $newArtistTd.text(artist);
 
     let $newRatingTd = $('<td>');
+    $newRatingTd.addClass('text-center rating');
     $newRatingTd.text(rating);
 
     let $newBtnTd = $('<td>');
-    let $newButton = $('<button>');
-    $newButton.attr('type', 'submit');
-    $newButton.addClass('btn btn-warning');
-    $newButton.text('Delete');
+    $newBtnTd.addClass('text-center delete');
+    let $newButton = createDeleteBtn();
     $newBtnTd.append($newButton);
 
     let $newRow = $('<tr>');
@@ -31,9 +30,23 @@ $(function() {
     $newRow.append($newRatingTd);
     $newRow.append($newBtnTd);
 
-    $('table').append($newRow);
+    $('tbody').append($newRow);
 
-    $('#artist').text('');
-    $('#rating').text('');
+    $('#artist').val('');
+    $('#rating').val('');
+
+    $('#artist').focus();
+  }
+
+  function createDeleteBtn() {
+    let $newButton = $('<button>');
+    $newButton.attr('type', 'submit');
+    $newButton.addClass('btn btn-danger btn-block');
+    $newButton.text('Delete');
+    $newButton.on('click', function(event) {
+      $(event.target).parent().parent().remove();
+    });
+
+    return $newButton;
   }
 });
